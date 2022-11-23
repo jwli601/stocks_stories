23fb8962cae753d634506fad2c0c74cb7d24fa8c
jwli601 <58207250+jwli601@users.noreply.github.com>
2022-11-23 13:50:21 +0800

    Update working_table.py

--- working_table.py
+++ working_table.py
@@ -57,6 +57,6 @@ zmlc = dingpan_plots.zh_us_interests_diff()
 zmlc_z = (zmlc-zmlc.mean())/zmlc.std()
 sz50_predict = (zmlc_z.z[-1]*zmlc.std().sz50)+zmlc.mean().sz50
 print(sz50_predict)
-dingpan_plots.sz50_over_zz500(start_date='2015-01-01')
+dingpan_plots.sz50_over_zz500()
 
 print(dingpan_plots.zh_us_interests_diff())
