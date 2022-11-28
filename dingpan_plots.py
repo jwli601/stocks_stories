9a8b7abd13018b301d48f6904c58a5c400e28567
jwli601 <58207250+jwli601@users.noreply.github.com>
2022-11-28 13:33:15 +0800

    乱码了丫的

--- dingpan_plots.py
+++ dingpan_plots.py
@@ -1,29 +1,28 @@
-# -*- coding: gbk -*-
+﻿# -*- coding: utf-8 -*-
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-plt.rcParams['font.sans-serif']=['SimHei'] #ÓÃÀ´Õý³£ÏÔÊ¾ÖÐÎÄ±êÇ©
-plt.rcParams['axes.unicode_minus']=False #ÓÃÀ´Õý³£ÏÔÊ¾¸ººÅ
-import akshare as ak # akshare ÓÃÀ´¶ÁÈ¡µ±ÈÕµ±Ê±µÄÊý¾Ý£¬akshareÈç¹ûÓÃÀ´¶ÁÀúÊ·Êý¾Ý¾­³£»á³öÏÖÊý¾Ý¶ªÊ§ÏÖÏó£¬²»ºÃÓÃ
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+import akshare as ak # akshare 用来读取当日当时的数据，akshare如果用来读历史数据经常会出现数据丢失现象，不好用
 import tushare as ts
 import datetime
 import cv_strategy
-import matplotlib.gridspec as gridspec
 
 
 def sw_heatmap():
     date_today = str(datetime.date.today())
 
     sw_1st_info = ak.sw_index_first_info()
-    sw_1st_list = [i[:6]for i in sw_1st_info['ÐÐÒµ´úÂë']]
+    sw_1st_list = [i[:6]for i in sw_1st_info['行业代码']]
     sw_data = pd.DataFrame(columns=sw_1st_list)
     for i in sw_1st_list:
-        sw_data[i] = ak.index_hist_sw(i).set_index('ÈÕÆÚ')['ÊÕÅÌ']
+        sw_data[i] = ak.index_hist_sw(i).set_index('日期')['收盘']
 
-    sw_today = ak.index_realtime_sw('Ò»¼¶ÐÐÒµ').set_index('Ö¸Êý´úÂë')
-    sw_data.loc[date_today,:] = sw_today['×îÐÂ¼Û']
-    sw_data.columns = sw_1st_info['ÐÐÒµÃû³Æ']
+    sw_today = ak.index_realtime_sw('一级行业').set_index('指数代码')
+    sw_data.loc[date_today,:] = sw_today['最新价']
+    sw_data.columns = sw_1st_info['行业名称']
     sw_data.dropna(inplace=True)
     sw_data_v = ((sw_data-sw_data.rolling(50).mean())/(sw_data.rolling(50).std())).dropna()
     sw_data_v = sw_data_v.T
@@ -49,10 +48,10 @@ def zh_us_interests_diff(start_date=''):
     sz50 = ts.get_k_data('sz50',start=start_date).set_index('date')
     zz500 = ts.get_k_data('399905',start=start_date).set_index('date')
     bond_table = ak.bond_zh_us_rate()
-    bond_table.set_index('ÈÕÆÚ',inplace=True)
+    bond_table.set_index('日期',inplace=True)
     bond_table.index = [str(i) for i in bond_table.index]
-    bond_table['ÖÐÃÀÀû²î_10Y'] = bond_table['ÖÐ¹ú¹úÕ®ÊÕÒæÂÊ10Äê']-bond_table['ÃÀ¹ú¹úÕ®ÊÕÒæÂÊ10Äê']
-    zmlc_10Y = bond_table['ÖÐÃÀÀû²î_10Y'].dropna()
+    bond_table['中美利差_10Y'] = bond_table['中国国债收益率10年']-bond_table['美国国债收益率10年']
+    zmlc_10Y = bond_table['中美利差_10Y'].dropna()
     df1 = pd.DataFrame({'z':zmlc_10Y,'sz50':sz50.close,'hs300':hs300.close,'zz500':zz500.close})
     df1.dropna(inplace=True)
     df1['sz50_shift100'] = df1.sz50.shift(-100)
@@ -63,16 +62,16 @@ def zh_us_interests_diff(start_date=''):
     df1_1 = (df1-df1.mean())/df1.std()
     plt.figure(figsize=[10,6],dpi=100)
     plt.style.use('ggplot')
-    plt.title('ÖÐÃÀÀû²îÓëÉÏÖ¤50Ö¸Êý×ßÊÆ corr = {}'.format(round(corr,3)))
+    plt.title('中美利差与上证50指数走势 corr = {}'.format(round(corr,3)))
 
 
-    plt.plot(df1_1.z,color='red',label='ÖÐÃÀÀû²î')
-    plt.plot(df1_1.sz50,color='blue',alpha=0.2,label='ÉÏÖ¤50\n  corr={}'.format(round(corr,3)))
+    plt.plot(df1_1.z,color='red',label='中美利差')
+    plt.plot(df1_1.sz50,color='blue',alpha=0.2,label='上证50\n  corr={}'.format(round(corr,3)))
     plt.plot(df1_1.sz50.shift(-100),color='blue',label='sz50_shift_100\n  corr={}'.format(round(corr2,3)))
 
     plt.legend()
     plt.xticks(df1.index[::20],rotation=90,fontsize=8)
-    plt.ylabel('×ÝÖá¾­±ê×¼»¯',color='grey')
+    plt.ylabel('纵轴经标准化',color='grey')
     plt.show()
     
     return df1
@@ -90,7 +89,7 @@ def stock_bond_rolling(start_date=''):
 
     plt.xticks(df_rocz.index[::20],rotation=90)
     plt.legend()
-    plt.title('¹ÉÕ®ÂÖ¶¯Ä£ÐÍ corr = {}'.format(round(df_rocz.corr().loc['sz50','bond_index'],3)))
+    plt.title('股债轮动模型 corr = {}'.format(round(df_rocz.corr().loc['sz50','bond_index'],3)))
     plt.show()
     return df
 def sz50_over_zz500(start_date = ''):
@@ -101,26 +100,26 @@ def sz50_over_zz500(start_date = ''):
     df = df/df.iloc[0]
 
     plt.figure(figsize=[8,5],dpi=80)
-    plt.plot(df.sz50,alpha=.4,label='ÉÏÖ¤50')
-    plt.plot(df.zz500,alpha=.4,label='ÖÐÖ¤500')
-    # plt.plot(df['sz50/zz500'],color='green',label='ÉÏÖ¤50/ÖÐÖ¤500')
+    plt.plot(df.sz50,alpha=.4,label='上证50')
+    plt.plot(df.zz500,alpha=.4,label='中证500')
+    # plt.plot(df['sz50/zz500'],color='green',label='上证50/中证500')
     plt.xticks(df.index[::60],rotation=90)
-    plt.title('¹æÄ£Ð§Ó¦£ºÉÏÖ¤50/ÖÐÖ¤500')
+    plt.title('规模效应：上证50/中证500')
     plt.legend()
     plt.show()
 
     plt.figure(figsize=[8,5],dpi=80)
-    # plt.plot(df.sz50,alpha=.4,label='ÉÏÖ¤50')
-    # plt.plot(df.zz500,alpha=.4,label='ÖÐÖ¤500')
-    plt.plot(df['sz50/zz500'],color='green',label='ÉÏÖ¤50/ÖÐÖ¤500')
+    # plt.plot(df.sz50,alpha=.4,label='上证50')
+    # plt.plot(df.zz500,alpha=.4,label='中证500')
+    plt.plot(df['sz50/zz500'],color='green',label='上证50/中证500')
     plt.xticks(df.index[::60],rotation=90)
-    plt.title('¹æÄ£Ð§Ó¦£ºÉÏÖ¤50/ÖÐÖ¤500')
+    plt.title('规模效应：上证50/中证500')
     plt.legend()
     plt.show()
     
 def hs300_streamline():
     hs300_list = pd.read_excel(r'D:\jwli\stocks_stories\hs300.xlsx',sheet_name=1).dropna()
-    hs300_list = [i[:6] for i in hs300_list['Ö¤È¯´úÂë']]
+    hs300_list = [i[:6] for i in hs300_list['证券代码']]
     hs300_pool = cv_strategy.cv_strategy(hs300_list)
     hs300_pool.get_close()
     area = hs300_pool.close
@@ -164,7 +163,7 @@ def hs300_streamline():
     # y = rtn.iloc[-1][area_1]
     # plt.scatter(x=x,y=y)
     fig=plt.figure(figsize=[10,10],dpi=100)
-    fig.suptitle('»¦Éî300³É·Ö¹ÉÓãÈºÍ¼')
+    fig.suptitle('沪深300成分股鱼群图')
     colors = ['red','orange','orange','green']
     for j in range(1,5):
         a1 = (vel.iloc[-1][vel.iloc[-1]==j]).index
@@ -177,7 +176,7 @@ def hs300_streamline():
             ax.set_xlim(0,.2)
             ax.set_ylim(-.5,.5)
             ax.arrow(x=a1x[i],y=a1y[i],dx=a1dx[i],dy=a1dy[i],width=0.0005,head_width=0.005,color=colors[j-1])
-            ax.set_title('ÏòµÚ{}ÏóÏÞÒÆ¶¯:{}Ö»'.format(j,len(a1)))
+            ax.set_title('向第{}象限移动:{}只'.format(j,len(a1)))
 def run_all():
     sw_heatmap()
     zh_us_interests_diff()
